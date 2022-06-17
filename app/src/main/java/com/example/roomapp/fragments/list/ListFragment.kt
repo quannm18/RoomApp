05fb4810802c6c4df7d4bbf6c692dd666a69bdd7
thinhkaku <thinhkaku.a3k51@gmail.com>
2022-06-17 18:10:08 +0700

    COm

---
+++ app/src/main/java/com/example/roomapp/fragments/list/ListFragment.kt
@@ -0,0 +1,34 @@
+package com.example.roomapp.fragments.list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.roomapp.R
+import com.example.roomapp.data.UserViewModel
+import kotlinx.android.synthetic.main.fragment_list.view.*
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        view.floatingActionButton.setOnClickListener {
+//            Navigation.findNavController(view).navigate(R.id.fragment, null);
+            Navigation.findNavController(view).navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        return view
+    }
+
+}
